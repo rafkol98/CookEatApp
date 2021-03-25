e02cf672aca86fad148ee779a54d1e6015757151
rafaelkoll <rafcall98@hotmail.com>
2021-03-25 12:19:21 +0200

    add new recipe view

--- CookEat/View/Recipes/AddRecipeView.swift
+++ CookEat/View/Recipes/AddRecipeView.swift
@@ -9,14 +9,75 @@ import SwiftUI
 
 struct AddRecipeView: View {
     @State var name: String = ""
-    @State var address: String = ""
-    @State var email: String = ""
+    @State var ingredients: String = ""
+    @State var instructions: String = ""
+    @State var description: String = ""
+    
     var body: some View {
         VStack{
-            TextField("Recipe Name", text: $name)
-            TextEditor(text: $email)
+            SmallInput(name: "RecipeName", stringIn: $name)
+            LargeInput(name: "Description", stringIn: $description)
+            LargeInput(name: "Ingredients", stringIn: $ingredients)
+            LargeInput(name: "Instructions", stringIn: $instructions)
+            
+            Button(action: {
+            }, label: {
+                Text("Add Recipe")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .frame(width: 360, height: 50)
+                    .background(Color.red)
+                    .clipShape(Capsule())
+                    .padding()
+            })
+            
+        }.padding()
+        .foregroundColor(.black)
+        
+    }
+}
+
+struct SmallInput: View {
+    let name: String
+    @Binding
+    var stringIn: String
+    
+    var body: some View {
+        HStack{
+            Text(name)
+            Spacer()
         }
-     
+        TextField("", text: $stringIn)
+            .foregroundColor(Color.gray)
+            .font(.custom("HelveticaNeue", size: 14))
+            .padding()
+            .overlay(
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(Color.gray, lineWidth: 2)
+            )
+    }
+}
+
+
+struct LargeInput: View {
+    let name: String
+    @Binding
+    var stringIn: String
+    
+    var body: some View {
+        HStack{
+            Text(name)
+            Spacer()
+        }
+    
+        TextEditor(text:  $stringIn)
+            .foregroundColor(Color.gray)
+            .font(.custom("HelveticaNeue", size: 14))
+            .lineSpacing(5)
+            .overlay(
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(Color.gray, lineWidth: 2)
+            )
     }
 }
 

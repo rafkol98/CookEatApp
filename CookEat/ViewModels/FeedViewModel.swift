6a083b79a14d4f18129af82be6736a16306a0163
rafaelkoll <rafcall98@hotmail.com>
2021-03-29 18:50:40 +0300

    pro

--- CookEat/ViewModels/FeedViewModel.swift
+++ CookEat/ViewModels/FeedViewModel.swift
@@ -16,31 +16,27 @@ class FeedViewModel: ObservableObject {
     }
     
     func fetchRecipes(){
-//        COLLECTION_RECIPES.getDocuments { snapshot, _ in
-//            guard let documents = snapshot?.documents else { return }
-//
-//            self.recipes = documents.map({ Recipe(dictionary: $0.data()) })
-//        }
+        //        COLLECTION_RECIPES.getDocuments { snapshot, _ in
+        //            guard let documents = snapshot?.documents else { return }
+        //
+        //            self.recipes = documents.map({ Recipe(dictionary: $0.data()) })
+        //        }
         guard let userUid = Auth.auth().currentUser?.uid else { return }
-        let following_ref = COLLECTION_FOLLOWING.document(userUid).collection("user-following")
-        following_ref.getDocuments { snapshot, _ in
-            guard let following = snapshot?.documents else { return }
-            print(following)
+        
+        COLLECTION_RECIPES.addSnapshotListener { (querySnaphot, error) in
+            guard let documents = querySnaphot?.documents else { return }
             
-            COLLECTION_RECIPES.addSnapshotListener { (querySnaphot, error) in
-                guard let documents = querySnaphot?.documents else { return }
+            self.recipes = documents.map{ (queryDocumentSnapshot) -> Recipe in
+                let data = queryDocumentSnapshot.data()
+                let uid = data["uid"]
                 
-                self.recipes = documents.map{ (queryDocumentSnapshot) -> Recipe in
-                    let data = queryDocumentSnapshot.data()
-                    let uid = data["uid"]
-                    
                 
-                    print(uid ?? "no")
-                    let recipe = Recipe(dictionary: data)
-                    return recipe
-                }
-                
+                print(uid ?? "no")
+                let recipe = Recipe(dictionary: data)
+                return recipe
             }
+            
+            
             
         }
         

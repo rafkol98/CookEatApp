b4740d4eb623e250b66b1eaf7fc99dfcd59e3c6e
rafaelkoll <rafcall98@hotmail.com>
2021-07-23 12:55:01 +0300

    Backend time-version
    almost

--- CookEat/ViewModels/AcceptRejectViewModel.swift
+++ CookEat/ViewModels/AcceptRejectViewModel.swift
@@ -20,24 +20,35 @@ class AcceptRejectViewModel: ObservableObject {
     func accept() {
         //Delete the contribution from the receive and update the original recipe.
         COLLECTION_USERS.document(contribution.originalUid).collection("received").document(contribution.id).delete(completion: { _ in
+            let data : [String: Any] = [
+                "addedIngredients": self.contribution.addedIngredients,
+                "addedInstructions": self.contribution.addedInstructions,
+                "removedIngredients": self.contribution.removedIngredients,
+                "removedInstructions": self.contribution.removedInstructions,
+                "id": self.contribution.id,
+                "fullname": self.contribution.fullname,
+                "profileImageUrl": self.contribution.profileImageUrl,
+                "originalIngredients": self.contribution.originalIngredients,
+                "originalInstructions": self.contribution.removedInstructions
+            ]
             
-            COLLECTION_RECIPES.document(self.contribution.originalId).getDocument{ snapshot, _ in
-                guard let data = snapshot?.data() else { return }
-                
-                COLLECTION_RECIPES.document(self.contribution.originalId).updateData([
-                    "ingredients": self.contribution.suggestedIngredients,
-                    "instructions": self.contribution.suggestedInstructions,
-                    "history": data
-                ]) { error in
-                    if let error = error {
-                        print("Error updating document: \(error)")
-                    } else {
-                        print("Document successfully updated")
-                    }
+            // Get time - used for time version.
+            let formatter = DateFormatter()
+            formatter.dateStyle = .long
+            let dateString = formatter.string(from: Date())
+            
+            COLLECTION_RECIPES.document(self.contribution.originalId).updateData([
+                "ingredients": self.contribution.suggestedIngredients,
+                "instructions": self.contribution.suggestedInstructions
+            ]) { error in
+                if let error = error {
+                    print("Error updating document: \(error)")
+                } else {
+                    // Store history of contribution - time version.
+                    COLLECTION_RECIPES.document(self.contribution.originalId).collection("history").document(dateString).setData(data)
+                    print("Document successfully updated")
                 }
             }
-            
-           
         })
         
         // Suggested recipe that the user suggested update status to approved.
